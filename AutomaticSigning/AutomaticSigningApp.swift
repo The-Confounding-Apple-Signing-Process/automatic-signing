123fc4594970eb2018f95d558fcf5212dd38cca6
Uday Pandey <uday.pandey@gmail.com>
2021-11-05 10:36:35 +0000

    Added sample project

---
+++ AutomaticSigning/AutomaticSigningApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomaticSigningApp.swift
+//  AutomaticSigning
+//
+//  Created by Uday Pandey on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct AutomaticSigningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
